4e8bf328b0fcca8632d295aab269771ff2bf82ad
Farida15 <82628049+Farida15@users.noreply.github.com>
2021-04-30 17:55:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ cnv.height = displayHeight;
 document.getElementById("color").oninput = function(){
     color = this.value;
 }
+document.getElementById("size").oninput = function(){
+    width = this.value;
+}
 
 cnv.onmousedown = (e) => {
     setTimeout(() => {
